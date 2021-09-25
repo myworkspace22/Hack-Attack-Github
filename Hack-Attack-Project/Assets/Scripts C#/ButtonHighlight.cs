fb2fbcc1ecfa211e164037e1b3ddcaad1b0c3208
Ribbo <61548182+myworkspace22@users.noreply.github.com>
2021-09-25 13:41:46 +0200

    Fixing Interface

--- Hack-Attack-Project/Assets/Scripts C#/ButtonHighlight.cs
+++ Hack-Attack-Project/Assets/Scripts C#/ButtonHighlight.cs
@@ -20,18 +20,18 @@ public class ButtonHighlight : MonoBehaviour
     }
     private void DisableButtons()
     {
-        shooter.color = new Color(0.10f, 0.10f, 0.10f);
-        cannon.color = new Color(0.10f, 0.10f, 0.10f);
-        laser.color = new Color(0.10f, 0.10f, 0.10f);
+        shooter.color = new Color(0.08f, 0.08f, 0.08f);
+        cannon.color = new Color(0.08f, 0.08f, 0.08f);
+        laser.color = new Color(0.08f, 0.08f, 0.08f);
         shooterIcon.color = new Color(0.50f, 0.50f, 0.50f);
         cannonIcon.color = new Color(0.50f, 0.50f, 0.50f);
         laserIcon.color = new Color(0.50f, 0.50f, 0.50f);
     }
     private void EnableButtons()
     {
-        shooter.color = new Color(0.10f, 0.10f, 0.10f);
-        cannon.color = new Color(0.10f, 0.10f, 0.10f);
-        laser.color = new Color(0.10f, 0.10f, 0.10f);
+        shooter.color = new Color(0.08f, 0.08f, 0.08f);
+        cannon.color = new Color(0.08f, 0.08f, 0.08f);
+        laser.color = new Color(0.08f, 0.08f, 0.08f);
         shooterIcon.color = new Color(1, 1, 1);
         cannonIcon.color = new Color(1, 1, 1);
         laserIcon.color = new Color(1, 1, 1);
@@ -50,9 +50,9 @@ public class ButtonHighlight : MonoBehaviour
         
         selectedTurret = buildmanager.GetTurretToBuild();
 
-        shooter.color = new Color(0.10f, 0.10f, 0.10f);
-        cannon.color = new Color(0.10f, 0.10f, 0.10f);
-        laser.color = new Color(0.10f, 0.10f, 0.10f);
+        shooter.color = new Color(0.08f, 0.08f, 0.08f);
+        cannon.color = new Color(0.08f, 0.08f, 0.08f);
+        laser.color = new Color(0.08f, 0.08f, 0.08f);
         shooterIcon.color = new Color(1, 1, 1);
         cannonIcon.color = new Color(1, 1, 1);
         laserIcon.color = new Color(1, 1, 1);
@@ -61,16 +61,16 @@ public class ButtonHighlight : MonoBehaviour
         {
             if(selectedTurret.title == "Shooter")
             {
-                shooter.color = new Color(0.15f, 0.15f, 0.15f);
+                shooter.color = new Color(0.12f, 0.12f, 0.12f);
             }else
             if (selectedTurret.title == "Missile")
             {
-                cannon.color = new Color(0.15f, 0.15f, 0.15f);
+                cannon.color = new Color(0.12f, 0.12f, 0.12f);
             }
             else
             if (selectedTurret.title == "Laser")
             {
-                laser.color = new Color(0.15f, 0.15f, 0.15f);
+                laser.color = new Color(0.12f, 0.12f, 0.12f);
             }
         }
     }
